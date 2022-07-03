d68f06d2eb0938bdd5a4ddfe24e19a3c57eb07e4
YangJun <179101536@qq.com>
2022-07-04 01:02:32 +0800

    Update settings.py

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -143,7 +143,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_HOST = 'smtp.163.com'   # 用于发送电子邮件的主机
 EMAIL_HOST_USER = "xxxxxxx@163.com"    # 自己的邮箱地址
-EMAIL_HOST_PASSWORD = "LBQXWTYHLTADIOXJ"       # 自己的邮箱密码
+EMAIL_HOST_PASSWORD = ""       # 自己的邮箱密码
 EMAIL_PORT = 465                     # 用于中定义的SMTP服务器的端口
 EMAIL_USE_SSL = True             # 是否使用隐式的安全连接
 
