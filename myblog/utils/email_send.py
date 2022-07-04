8b0f28805373df76521e851e891a77d840c5c946
YangJun <179101536@qq.com>
2022-07-04 09:35:59 +0800

    Update email_send.py

--- myblog/utils/email_send.py
+++ myblog/utils/email_send.py
@@ -22,13 +22,13 @@ def send_register_email(email, send_type='register'):
     if send_type == 'register':
         email_title = '博客的注册激活链接'
         email_body = '请点击以下链接激活账号: http://127.0.0.1:8000/users/active/{0}'.format(code)
-        send_status = send_mail(email_title, email_body, 'y13115022360@163.com', [email])
+        send_status = send_mail(email_title, email_body, 'xxxxxxxxxxxx@163.com', [email]) # 填写您的发送邮箱地址
         if send_status:
             pass
 
     elif send_type == 'forget':
         email_title = '找回密码链接'
         email_body = '请点击以下链接修改密码: http://127.0.0.1:8000/users/forget_pwd_url/{0}'.format(code)
-        send_status = send_mail(email_title, email_body, 'y13115022360@163.com', [email])
+        send_status = send_mail(email_title, email_body, 'xxxxxxxxxx@163.com', [email])  # 填写您的发送邮箱地址
         if send_status:
             pass
